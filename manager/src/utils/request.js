e4a16e3940d678bbeec6ecb5ddf15c38c71b845a
zhangxinxin3 <46130188+zhangxinxin3@users.noreply.github.com>
2019-06-12 07:40:14 +0800

    Update request.js

--- manager/src/utils/request.js
+++ manager/src/utils/request.js
@@ -1,13 +1,10 @@
 import axios from 'axios'
 
-// create an axios instance
 const service = axios.create({
   baseURL: 'http://169.254.12.250:7001/',
-  // withCredentials: true, // 跨域请求时发送 cookies
-  timeout: 5000 // request timeout
+  timeout: 5000
 })
 
-// request interceptor
 service.interceptors.request.use(
   config => {
     return config
